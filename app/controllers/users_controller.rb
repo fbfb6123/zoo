f4198e7339d47f156fecd2563b9837490bb8fe3b
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-04-03 21:02:18 +0900

    mypage調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_current_user
+
   def show
   end
 
@@ -15,10 +17,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name,:phone_number,:city, :city_adress,:name_kana,:encrypted_password,:birthday,:icon,:introduction)
   end
 
 end
