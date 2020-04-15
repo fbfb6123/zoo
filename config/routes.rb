b0e622513614a9acd6339bcc06c0b013892cda2a
kanekoshinya <knksny777@icloud.com>
2020-04-15 21:40:22 +0900

    検索機能初期段階

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :dogs, only: [:index]
   resources :cats, only: [:index]
+  resources :areas, only: [:index]
   resources :facilities, only: [:index,:new,:create,:show, :edit, :update] do
     resources :animals, only: [:index,:new,:create,:show, :edit, :update]
   end
