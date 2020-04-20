2a1f12a1c8a7ac8c8ccf67388625283ea5562a62
kanekoshinya <knksny777@icloud.com>
2020-04-21 01:14:33 +0900

    施設お気に入り機能

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :dogs, only: [:index,:new,:create]
   resources :events, only: [:index,:new,:create,:show]
   resources :facilities, only: [:index,:new,:create,:show, :edit, :update] do
+    resources :likes, only: [:create, :destroy]
     resources :animals, only: [:index,:new,:create,:edit, :update]
   end
   resources :animals, only: [:show]
