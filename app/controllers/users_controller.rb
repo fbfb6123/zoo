b724997992ad7bba9d4a79f1dd2bfb8dbae4b9ed
kanekoshinya <knksny777@icloud.com>
2020-04-22 23:09:45 +0900

    お気に入り一覧ページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  
+  require 'will_paginate/array'
   before_action :set_current_user
 
   def index
     @facilities = Like.where(user_id: current_user.id).pluck(:facility_id)
     @facility = Facility.find(@facilities)
     @favorites = Favorite.where(user_id: current_user.id).pluck(:animal_id)
-    @favorite = Animal.find(@favorites)
+    @favorite = Animal.find(@favorites).paginate(page: params[:page], per_page: 4)
   end
 
   def show
