d1bd30f52d3a0de81088cc2490da4659be8e10ca
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-04-24 06:21:16 +0900

    tops/review

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,5 +4,7 @@ class TopsController < ApplicationController
     @event = Event.limit(3).order('id DESC')
     @dog = Animal.where(species: 0).limit(4).order('id ASC')
     @cat = Animal.where(species: 1).limit(4).order('id ASC')
+    @review = Review.limit(3).order('id DESC')
+    
   end
 end
