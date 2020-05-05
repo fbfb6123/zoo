a61ab2aaf72e84081c6e08da189b2e4bf82a8237
kanekoshinya <knksny777@icloud.com>
2020-05-05 21:56:58 +0900

    トッププページbuyer_id編集

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,9 +1,9 @@
 class TopsController < ApplicationController
   def index
-    @animal = Animal.limit(4).order('id DESC')
+    @animal = Animal.where(buyer_id: nil).limit(4).order('id DESC')
     @event = Event.limit(3).order('id DESC')
-    @dog = Animal.where(species: 0).limit(4).order('id ASC')
-    @cat = Animal.where(species: 1).limit(4).order('id ASC')
+    @dog = Animal.where(species: 0,buyer_id: nil).limit(4).order('id ASC')
+    @cat = Animal.where(species: 1,buyer_id: nil).limit(4).order('id ASC')
     @review = Review.limit(3).order('id DESC')
     
   end
