dc6554077bc428163709c2d44e6aae0057c315f4
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-05-08 20:15:58 +0900

    cat.dog_controller修正

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,5 @@
 class CatsController < ApplicationController
   def index
-    @cat = Animal.where(species:1).paginate(page: params[:page], per_page: 5)
+    @cat = Animal.where(species:1, buyer_id: nil).paginate(page: params[:page], per_page: 8)
   end
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,5 @@
 class DogsController < ApplicationController
   def index
-    @dog = Animal.where(species:0).paginate(page: params[:page], per_page: 8)
+    @dog = Animal.where(species:0, buyer_id: nil).paginate(page: params[:page], per_page: 8)
   end
 end
