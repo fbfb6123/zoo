a6a536846cc7c311ff1b0f53bea5c6cac63c35a5
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-05-08 20:44:50 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,7 @@
 class TopsController < ApplicationController
   def index
     @animal = Animal.where(buyer_id: nil).limit(4).order('id DESC')
-    @event = Event.limit(3).order('id ASC')
+    @event = Event.limit(3).order('id DESC')
     @dog = Animal.where(species: 0,buyer_id: nil).limit(4).order('id ASC')
     @cat = Animal.where(species: 1,buyer_id: nil).limit(4).order('id ASC')
     @review = Review.limit(3).order('id DESC')
